d9683b709b538d2967a11273b15a59730bdeabc8
Andrew Gordienko <levanevskogo79@gmail.com>
2023-02-24 13:07:08 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,16 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslations } from
-    'shared/lib/tests/renderWithTranslations/renderWithTranslations';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('test render', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleButton = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleButton);
