519f7b2185db08d74104304ac1a8a2088adc32da
Andrew Gordienko <levanevskogo79@gmail.com>
2023-02-28 22:09:38 +0300

    fix Modal and global styles

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,6 +1,6 @@
-import { CounterScheme } from 'entities/Counter/model/types/CounterScheme';
-import { counterReducer } from 'entities/Counter/model/slice/CounterSlice';
-import { Counter } from 'entities/Counter/ui/Counter';
+import { CounterScheme } from './model/types/CounterScheme';
+import { counterReducer } from './model/slice/CounterSlice';
+import { Counter } from './ui/Counter';
 
 export {
     CounterScheme,
