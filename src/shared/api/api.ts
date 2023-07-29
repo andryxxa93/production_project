f6bf23f83cb379dad68f1d99ad76b3a90034ca70
Andrew Gordienko <levanevskogo79@gmail.com>
2023-07-29 13:13:32 +0300

    change apiUrl

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,12 @@ const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : 'https://production.ru';
 
 export const $api = axios.create({
     baseURL: baseUrl,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+
+    return config;
 });
